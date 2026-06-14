84bd3bfa68fd592147cdaf3d0c01eb947abfb9a1
Bartosz Budzynowski <bartoszbud@MacBook-Pro-Bartosz.local>
2026-06-14 18:25:54 +0200

    feat: add dns lxc instances

---
+++ terraform/envs/prod/variables.tf
@@ -0,0 +1,62 @@
+/*variable "environment" {
+  description = "Environment"
+  type        = string
+}*/
+
+variable "pm_api_url" {
+  description = "Proxmox API URL"
+  type        = string
+}
+
+variable "pm_user" {
+  description = "Proxmox User"
+  type        = string
+}
+
+variable "pm_api_token_id" {
+  description = "Proxmox API Token ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "pm_api_token_secret" {
+  description = "Proxmox API Token Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "pm_tls_insecure" {
+  description = "Proxmox TLS Insecure"
+  type        = bool
+  default     = true
+}
+
+variable "lxc_instances" {
+  description = "Map of LXC instances to create"
+  type = map(object({
+    target_node  = string
+    start        = bool
+    onboot       = bool
+    description  = string
+    tags         = string
+    vmid         = number
+    hostname     = string
+    ostemplate   = string
+    password     = string
+    cores        = number
+    memory       = number
+    nameserver   = string
+    searchdomain = string
+    rootfs = object({
+      storage = string
+      size    = string
+    })
+    network = object({
+      name   = string
+      bridge = string
+      ip     = string
+      gw     = string
+    })
+    ssh_public_keys = string
+  }))
+}
